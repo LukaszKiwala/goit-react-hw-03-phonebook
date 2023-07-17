324e88e90e0263f888858bee987f756de297d198
LukaszKiwala <113800025+LukaszKiwala@users.noreply.github.com>
2023-07-17 17:49:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,7 @@ import Notification from './Notification';
 
 class App extends Component {
   state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -112,74 +107,4 @@ class App extends Component {
   }
 }
 
-import React, { Component } from 'react';
-import s from './ContactForm.module.css';
-
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
-
-  handleChange = name => e => {
-    const { target } = e;
-
-    this.setState(() => ({
-      [name]: target.value,
-    }));
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-
-    const { onSubmit } = this.props;
-    onSubmit(this.state);
-    this.resetForm();
-  };
-
-  resetForm = () => {
-    this.setState(() => ({
-      name: '',
-      number: '',
-    }));
-  };
-
-  render() {
-    return (
-      <form className={s.form} onSubmit={this.handleSubmit}>
-        <label>
-          Name
-          <input
-            className={s.inputName}
-            value={this.state.name}
-            onChange={this.handleChange('name')}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-        <label>
-          Number
-          <input
-            className={s.inputNumber}
-            value={this.state.number}
-            onChange={this.handleChange('number')}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </label>
-
-        <button type="submit" className={s.buttonEditor}>
-          Add contact
-        </button>
-      </form>
-    );
-  }
-}
-
-export default ContactForm;
+export default App;
